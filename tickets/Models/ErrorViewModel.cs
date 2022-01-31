6efeb979cecc282078e3c225e3ef95ec60aaf9b6
Florim <Florim@DESKTOP-BMJ7T2S>
2022-01-31 20:31:43 +0100

    Add project files.

---
+++ tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
